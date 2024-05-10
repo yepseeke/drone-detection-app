7f2030fa5e412791b948009d92f0929bfd89e36a
Mirali77 <79355083+Mirali77@users.noreply.github.com>
2024-05-10 14:53:46 +0300

    Added image monitor + connected image buttons to it

---
+++ imageMonitorWidget.py
@@ -0,0 +1,30 @@
+from PyQt5 import uic, QtCore
+from PyQt5.QtWidgets import QWidget, QLabel
+
+class ImageMonitorWidget(QWidget):
+    def __init__(self):
+        super(ImageMonitorWidget, self).__init__()
+        uic.loadUi('imageMonitorWidget.ui', self)
+        self.label = self.findChild(QLabel, "label")
+        self.label.setMinimumSize(1, 1)
+        self.setStyleSheet("background-color: black;")
+        self.imageLoaded = False
+        self.image = None
+
+    def setImage(self, loadedImage):
+        self.imageLoaded = True
+        self.image = loadedImage
+        scaledImage = self.image.scaled(self.label.width(), self.label.height(), QtCore.Qt.KeepAspectRatio)
+        self.label.setPixmap(scaledImage)
+
+    def resizeEvent(self, event):
+        if not self.imageLoaded:
+            return
+        scaledImage = self.image.scaled(self.label.width(), self.label.height(), QtCore.Qt.KeepAspectRatio)
+        self.label.setPixmap(scaledImage)
+        QWidget.resizeEvent(self, event)
+
+    def clear(self):
+        self.imageLoaded = False
+        self.image = None
+        self.label.clear()
--- mainWindow.py
+++ mainWindow.py
@@ -1,8 +1,30 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QMainWindow, QHBoxLayout, QPushButton, QFileDialog
+from imageMonitorWidget import *
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        uic.loadUi('mainwindow.ui', self)
-        
+        uic.loadUi('mainWindow.ui', self)
+        self.imageLayout1 = self.findChild(QHBoxLayout, "horizontalLayout_4")
+        self.imageMonitorWidget = ImageMonitorWidget()
+        self.imageLayout1.addWidget(self.imageMonitorWidget, 13)
+
+        self.uploadImageButton = self.findChild(QPushButton, "uploadImageButton")
+        self.uploadImageButton.clicked.connect(self.uploadImage)
+
+        self.clearImageMonitorButton = self.findChild(QPushButton, "clearImageMonitorButton")
+        self.clearImageMonitorButton.clicked.connect(self.clearImageMonitor)
+
+    def uploadImage(self):
+        filepath = QFileDialog.getOpenFileName(self, "Укажите путь к картинке", "/home",
+                       "All files (*);; Jpg Files (*.jpg) ;; PNG Files (*.png);")[0]
+        if len(filepath) < 3 or filepath[len(filepath) - 3: len(filepath)] != "jpg" and filepath[len(filepath) - 3: len(filepath)] != "png":
+            print("Not image")
+            return
+        pic = QPixmap(filepath)
+        self.imageMonitorWidget.setImage(pic)
+
+    def clearImageMonitor(self):
+        self.imageMonitorWidget.clear()
