a53eb9358f5d4943f27248d6e63ba0dc80982d18
yepseeke <zolder.kaps@gmail.com>
2024-05-10 15:59:49 +0300

    Added method to get fps value

--- video_processing.py
+++ video_processing.py
@@ -9,12 +9,12 @@ class VideoProcessor:
     def __init__(self, source):
         self.video_capture = cv2.VideoCapture(source)
         self.net_model = None
-        self.current_frame = 0
+        self.current_frame_index = 0
 
     def get_frame(self, color_model='RGB'):
         success, frame = self.video_capture.read()
         if success:
-            self.current_frame += 1
+            self.current_frame_index += 1
             if color_model == 'BGR':
                 return frame
             return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -32,9 +32,10 @@ class VideoProcessor:
         return None
 
     def set_frame_index(self, frame_index: int):
-        if frame_index >= self.total_frames or frame_index < 0:
+        if frame_index < 0 or frame_index >= self.total_frames:
             raise Exception("Frame index is out of range")
         self.video_capture.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
+        self.current_frame_index = frame_index
 
     @property
     def total_frames(self):
@@ -53,3 +54,9 @@ class VideoProcessor:
         if not self.video_capture.isOpened():
             raise Exception("Error: Could not open video file.")
         return int(self.video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    @property
+    def fps(self):
+        if not self.video_capture.isOpened():
+            raise Exception("Error: Could not open video file.")
+        return self.video_capture.get(cv2.CAP_PROP_FPS)
