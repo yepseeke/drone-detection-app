15d193d1284a196cde35e607b66fe8648adfa275
yepseeke <zolder.kaps@gmail.com>
2024-05-10 21:57:08 +0300

    Fixed video processing

--- video_processing.py
+++ video_processing.py
@@ -16,24 +16,24 @@ class VideoProcessor:
         if success:
             self.current_frame_index += 1
             if color_model == 'BGR':
-                return frame
-            return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        return None
+                return success, frame
+            return success, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        return success, frame
 
     # function returns annotated frame
     def find_objects(self, frame):
         pass
 
     def get_annotated_frame(self, color_model='RGB'):
-        frame = self.get_frame(color_model)
-        if frame:
+        success, frame = self.get_frame(color_model)
+        if success:
             annotated_frame = self.find_objects(frame)
-            return annotated_frame
-        return None
+            return success, annotated_frame
+        return success, frame
 
     def set_frame_index(self, frame_index: int):
         if frame_index < 0 or frame_index >= self.total_frames:
-            raise Exception("Frame index is out of range")
+            raise Exception("Error: Frame index is out of range")
         self.video_capture.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
         self.current_frame_index = frame_index
 
@@ -56,7 +56,10 @@ class VideoProcessor:
         return int(self.video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
     @property
-    def fps(self):
+    def frame_rate(self):
         if not self.video_capture.isOpened():
             raise Exception("Error: Could not open video file.")
         return self.video_capture.get(cv2.CAP_PROP_FPS)
+
+    def release(self):
+        self.video_capture.release()
