8b0e8efd14fc418a4c8f97018fbdac63717eac43
yepseeke <zolder.kaps@gmail.com>
2024-05-14 15:41:40 +0300

    added fft plot

--- plot_widget.py
+++ plot_widget.py
@@ -5,7 +5,7 @@ from PyQt5 import QtCore
 from PyQt5.QtWidgets import QWidget, QMainWindow, QVBoxLayout
 from PyQt5.QtWidgets import QApplication
 
-from signal_processing import SignalProcessorFromFile
+from signal_processing import SignalProcessorFromAudioFile, SignalProcessorFromVideoFile, SignalProcessor
 
 
 class WidgetPlot(QWidget):
@@ -30,26 +30,22 @@ class WidgetPlot(QWidget):
 
 
 class SignalPlot(WidgetPlot):
-    def __init__(self, signal_source: str, window_size: int, background_color, styles, x_label: str,
-                 y_label: str,
-                 pen_color):
+    def __init__(self, audio_processor: SignalProcessor, window_size: int, background_color, styles, x_label: str,
+                 y_label: str, pen_color):
         super().__init__(background_color, styles, x_label, y_label, pen_color)
 
         self.window_size = window_size
 
-        self.audio_processor = SignalProcessorFromFile(signal_source)
-        self.audio_processor.read_signal()
+        self.audio_processor = audio_processor
 
         self.time = np.linspace(0, self.window_size, window_size)
         self.values = np.zeros(self.window_size)
 
         self.line = self.plot_graph.plot(self.time, self.values, pen=self.pen)
 
-    def update_values(self, time_start, time_end):
-        left = int(time_start * self.audio_processor.sample_rate)
-        right = int(time_end * self.audio_processor.sample_rate)
-
-        data = self.audio_processor.audio_data[left:right]
+    def update_values(self, start_time, end_time):
+        self.audio_processor.read_signal(start_time, end_time)
+        data = self.audio_processor.get_audio_data()
 
         if data.shape[0] < self.window_size:
             data = np.pad(data, (0, self.window_size - len(data)), mode='constant', constant_values=0)
@@ -58,6 +54,38 @@ class SignalPlot(WidgetPlot):
 
     def update_plot(self):
         self.line.setData(self.time, self.values)
+
+
+class FFTPlot(WidgetPlot):
+    def __init__(self, audio_processor: SignalProcessor, interval: int, background_color, styles, x_label: str,
+                 y_label: str, pen_color, plot_type='log'):
+        super().__init__(background_color, styles, x_label, y_label, pen_color)
+
+        self.audio_processor = audio_processor
+        self.interval = interval
+
+        sample_rate = self.audio_processor.sample_rate
+
+        # Creating array of frequencies
+        print(sample_rate)
+        self.freqs = np.linspace(0, sample_rate // 2, interval // 2)
+
+        # Defining zero values for the amplitude spectrum
+        self.values = np.zeros(interval // 2)
+
+        if plot_type == 'log':
+            self.plot_graph.setYRange(-180, 60)
+            self.plot_graph.setLogMode(x=True)
+
+        # Plot data
+        self.line = self.plot_graph.plot(self.freqs, self.values, pen=self.pen)
+
+    def update_values(self, time_start, time_end):
+        self.freqs = self.audio_processor.get_fft_data()[:self.interval]
+
+    def update_plot(self):
+        self.plot_graph.setYRange(-10, 20)
+        self.line.setData(self.freqs, self.values)
 
 
 class MyApp(QMainWindow):
@@ -71,14 +99,25 @@ class MyApp(QMainWindow):
         signal_y_label = "Amplitude"
         signal_background = (18, 74, 168)
 
+        fft_styles = {"color": "black", "font-size": "15px"}
+        fft_x_label = "Frequency [Hz]"
+        fft_y_label = "PSD [dB]"
+        fft_background = (255, 161, 0)
+
         pen_color = (0, 0, 0)
 
         central_widget = QWidget()
         layout = QVBoxLayout(central_widget)
 
-        self.signal_widget = SignalPlot(signal_source, window_size, signal_background, signal_styles,
+        self.audio_processor = SignalProcessorFromVideoFile(signal_source)
+
+        self.signal_widget = SignalPlot(self.audio_processor, window_size, signal_background, signal_styles,
                                         signal_x_label, signal_y_label, pen_color)
+        self.fft_widget = FFTPlot(self.audio_processor, window_size, fft_background, fft_styles, fft_x_label,
+                                  fft_y_label, pen_color, plot_type='log')
+
         layout.addWidget(self.signal_widget)
+        layout.addWidget(self.fft_widget)
 
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
@@ -95,16 +134,18 @@ class MyApp(QMainWindow):
     def update_data(self):
         # Adding signal values to display
         self.signal_widget.update_values(self.current_time, self.current_time + self.interval / self.sample_rate)
+        self.fft_widget.update_values(self.current_time, self.current_time + self.interval / self.sample_rate)
         self.current_time += self.interval / self.sample_rate
 
     def update_plot(self):
         self.update_data()
         self.signal_widget.update_plot()
+        self.fft_widget.update_plot()
 
 
 if __name__ == '__main__':
     app = QApplication([])
-    main = MyApp(r'minus.mp3')
+    main = MyApp(r'8.mp4')
     main.show()
 
     app.exec()
--- signal_processing.py
+++ signal_processing.py
@@ -1,18 +1,32 @@
 import librosa
+import moviepy.editor as mp
+import numpy as np
 
-from PyQt5 import QtCore
-from PyQt5.QtWidgets import QWidget, QMainWindow, QVBoxLayout
-from PyQt5.QtWidgets import QApplication
-from scipy import signal
+from scipy.fft import fft
 
 
 class SignalProcessor:
     def __init__(self):
         self.sample_rate = None
-        self.audio_data = None
+        self.duration = 0
+        self.audio_data = []
+        self.fft_data = []
+
+    def get_sample_rate(self):
+        return self.sample_rate
+
+    def get_audio_data(self):
+        return np.array(self.audio_data)
+
+    def get_fft_data(self):
+        return np.array(self.fft_data)
+
+    def get_length(self):
+        return len(self.audio_data)
 
 
-class SignalProcessorFromFile(SignalProcessor):
+# Currently doesn't work
+class SignalProcessorFromAudioFile(SignalProcessor):
     def __init__(self, audio_source):
         super().__init__()
 
@@ -21,16 +35,53 @@ class SignalProcessorFromFile(SignalProcessor):
     def read_signal(self):
         self.audio_data, self.sample_rate = librosa.load(self.audio_source, sr=None)
 
-    def get_length(self):
-        return self.audio_data.shape[0]
-
-    def get_sample_rate(self):
-        return self.sample_rate
-
-    def get_audio_data(self):
-        return self.audio_data
-
     def get_sample(self, sample_start_time, sample_end_time):
         left = int(sample_start_time / 1000 * self.sample_rate)
         right = int(sample_end_time / 1000 * self.sample_rate)
-        return self.audio_data[left:right]
+
+        if left < 0 or right >= len(self.audio_data):
+            raise IndexError('Error: Timings are out of range.')
+
+        return np.array(self.audio_data[left:right])
+
+
+class SignalProcessorFromVideoFile(SignalProcessor):
+    def __init__(self, video_source):
+        super().__init__()
+
+        self.video_source = video_source
+        self.video = mp.VideoFileClip(self.video_source)
+
+        self.duration = 0
+
+    def read_signal(self, start_time, end_time):
+        try:
+            audio = self.video.audio
+            self.sample_rate = audio.fps
+            self.duration = self.video.duration
+
+            if start_time < 0 or start_time > self.duration:
+                raise ValueError("Time exceeds video duration")
+
+            self.audio_data = audio.subclip(start_time, end_time).to_soundarray()
+            self.audio_data = np.mean(self.audio_data, axis=1)
+
+            interval = (end_time - start_time) * self.sample_rate
+            self.fft_signal(interval)
+
+        except Exception as error:
+            print('Error extracting audio from video:', error)
+
+    def fft_signal(self, interval, output_type='dB'):
+        epsilon = 1e-7
+        self.fft_data = np.abs(fft(self.audio_data, interval))[0:interval // 2] + epsilon
+        if output_type == 'dB':
+            self.fft_data = 20 * np.log10(self.fft_data)
+
+
+if __name__ == '__main__':
+    sp = SignalProcessorFromVideoFile('3.mp4')
+    sp.read_signal(109.89, 109.94)
+    arr = sp.get_audio_data()
+
+    print(arr)
